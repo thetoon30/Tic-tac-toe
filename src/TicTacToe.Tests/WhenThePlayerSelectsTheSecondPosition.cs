55dc4951aa6d39902e9aecec7fac8db88990c7de
toon <toon.b@excelbangkok.com>
2017-03-30 18:05:20 +0700

    - second position assert X test fail expect X but O

--- src/TicTacToe.Tests/WhenThePlayerSelectsTheSecondPosition.cs
+++ src/TicTacToe.Tests/WhenThePlayerSelectsTheSecondPosition.cs
@@ -10,7 +10,15 @@ namespace TicTacToe.Tests
         [Test]
         public void It_Should_Put_Their_Choice_In_The_Second_Position()
         {
-           
+            //Arrange
+            Game game = new Game();
+
+            //Act
+            game.ChoosePosition(2);
+            var position = game.GetPosition(2);
+
+            //Assert
+            Assert.AreEqual('X', position);
         }
     }
 }
