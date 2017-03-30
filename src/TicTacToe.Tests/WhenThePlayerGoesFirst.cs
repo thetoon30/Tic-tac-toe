be44c92eecd1bdb0059145ac79b79e51abc4fb19
toon <toon.b@excelbangkok.com>
2017-03-30 17:47:53 +0700

    - Arrange Act Assert Patterns

--- src/TicTacToe.Tests/WhenThePlayerGoesFirst.cs
+++ src/TicTacToe.Tests/WhenThePlayerGoesFirst.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
 using TicTacToe;
 
 namespace TicTacToe.Tests
@@ -10,9 +11,15 @@ namespace TicTacToe.Tests
         [Test]
         public void It_Should_Put_Their_Choice_In_The_Selected_Position()
         {
+            //Arrange
             var game = new Game();
+
+            //Act
             game.ChoosePosition(1);
-            Assert.AreEqual('X', game.GetPosition(1));
+            var position = game.GetPosition(1);
+
+            //Assert
+            Assert.AreEqual('X', position);
         }
 
         [Test]
