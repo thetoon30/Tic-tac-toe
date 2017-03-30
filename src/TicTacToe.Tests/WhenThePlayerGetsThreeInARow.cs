22933560626f6beff06a94a37d222e620f1d0d2a
toon <toon.b@excelbangkok.com>
2017-03-30 18:14:35 +0700

    - creating a new class and method

---
+++ src/TicTacToe.Tests/WhenThePlayerGetsThreeInARow.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class When_The_Player_Gets_Three_In_A_Row
+    {
+        [Test]
+        public void It_Should_Announce_The_Player_As_The_Winner()
+        {
+            
+        }
+    }
+}
