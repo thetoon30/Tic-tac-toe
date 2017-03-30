eb315a15e16ea438ad0bd76ca1fc3c36af7c1eb4
toon <toon.b@excelbangkok.com>
2017-03-30 18:02:20 +0700

    - creating a new class and method

---
+++ src/TicTacToe.Tests/WhenThePlayerSelectsTheSecondPosition.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class When_The_Player_Selects_The_Second_Position
+    {
+        [Test]
+        public void It_Should_Put_Their_Choice_In_The_Second_Position()
+        {
+           
+        }
+    }
+}
