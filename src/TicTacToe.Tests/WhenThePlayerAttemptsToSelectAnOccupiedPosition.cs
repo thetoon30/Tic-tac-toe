abeb0d315f2f2529a35445933dee0101091907b0
toon <toon.b@excelbangkok.com>
2017-03-31 09:56:07 +0700

    - new class for test When_the_player_attempts_to_select_an_occupied_position

---
+++ src/TicTacToe.Tests/WhenThePlayerAttemptsToSelectAnOccupiedPosition.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class When_the_player_attempts_to_select_an_occupied_position
+    {
+        [Test]
+        public void It_Should_Tell_The_Player_The_Position_Is_Occupied()
+        {
+        }
+    }
+}
